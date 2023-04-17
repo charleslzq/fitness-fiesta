b3094fddccdea4f11871f0d15656a6383bb7e0af
Eric Charles <github@ericcharleslzq.com>
2023-04-17 08:56:49 +0800

    add basic event type

---
+++ src/models/event.ts
@@ -0,0 +1,8 @@
+export type Event<
+    EventData extends Record<string, unknown> = Record<string, unknown>
+> = Readonly<{
+    id: Readonly<string>;
+    type: Readonly<string>;
+    timestamp: Readonly<Date>;
+    data: Readonly<EventData>;
+}>;
