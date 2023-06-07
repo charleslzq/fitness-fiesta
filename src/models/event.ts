0faa90e90505fd43b23a3bb82e98c6533ea80332
Eric Charles <github@ericcharleslzq.com>
2023-06-07 08:51:45 +0800

    remove redandunt code

--- src/models/event.ts
+++
@@ -1,8 +0,0 @@
-export type Event<
-  EventData extends Record<string, unknown> = Record<string, unknown>,
-> = Readonly<{
-  id: Readonly<string>;
-  type: Readonly<string>;
-  timestamp: Readonly<Date>;
-  data: Readonly<EventData>;
-}>;
